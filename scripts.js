e705f003543d1ec9401351c54947e73b728dd1ac
AlphieKiu <kiuhuaien87@gmail.com>
2019-10-05 18:54:58 -0500

    finished till bonus

---
+++ scripts.js
@@ -0,0 +1,73 @@
+// Write your JavaScript code here.
+
+
+
+function init() {
+    let takeOffButton = document.getElementById("takeoff");
+    let landingButton = document.getElementById("landing")
+    let missionAbortButton = document.getElementById("missionAbort")
+    let leftButton = document.getElementById("left")
+    let rightButton = document.getElementById("right")
+    let upButton = document.getElementById("up")
+    let downButton = document.getElementById("down")
+    let flightStatus = document.getElementById("flightStatus")
+    let shuttleBackgroundColor = document.getElementById("shuttleBackground")
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight")
+    let rocket = document.getElementById("rocket")
+    let rocketXPosition = null;
+
+    takeOffButton.addEventListener("click", function() {
+        if ((window.confirm("Confirm that the shuttle is ready for takeoff.")) === true) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackgroundColor.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = 10000;
+        }
+    })
+
+    landingButton.addEventListener("click", function() {
+        if ((window.confirm("The shuttle is landing. Landing gear engaged.")) === true) {
+            flightStatus.innerHTML = "The shuttle has landed.";
+            shuttleBackgroundColor.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = 0;
+        }
+    })
+
+    missionAbortButton.addEventListener("click", function() {
+        if ((window.confirm("Confirm that you want to abort the mission.")) === true) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackgroundColor.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = 0;
+        }
+    })
+
+
+
+    leftButton.addEventListener("click", function() {
+        rocketXPosition -= 10;
+        return rocket.style.left = rocketXPosition + "px";
+    });
+
+    rightButton.addEventListener("click", function() {
+        rocketXPosition += 10;
+        return rocket.style.left = rocketXPosition + "px";
+    });
+
+    downButton.addEventListener("click", function() {
+
+        rocketXPosition += 10;
+        rocket.style.top = rocketXPosition + "px";
+        if (spaceShuttleHeight.innerHTML >= 10000) {
+            spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) - 10000;
+        }
+
+    });
+
+    upButton.addEventListener("click", function() {
+
+        rocketXPosition -= 10;
+        rocket.style.top = rocketXPosition + "px";
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+    });
+}
+window.onload = init;
+// Remember to pay attention to page loading!
